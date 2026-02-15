29d21f53fe6223c6ff549c5420373cf0bf93bfdf
Eloise <141464740+eloise-normal-name@users.noreply.github.com>
2026-02-14 16:23:36 -0800

    Refactor headless mode initialization

--- src/main.cpp
+++ src/main.cpp
@@ -78,8 +78,7 @@ bool IsHeadlessMode() {
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, PSTR, int show_command) {
 	try {
-		auto headless = true;
-		// IsHeadlessMode();
+		auto headless = IsHeadlessMode();
 		auto hwnd = CreateAppWindow(instance, headless ? SW_HIDE : show_command);
 		if (!hwnd)
 			return 1;
